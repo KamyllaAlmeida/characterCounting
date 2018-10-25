576110cd165d5b33d421b11874c411cf4e3c8429
Kamylla Almeida <kamylla.florencio@gmail.com>
2018-10-25 15:25:19 +0000

    Function countLetters done

---
+++ countLetters.js
@@ -0,0 +1,16 @@
+function countLetters(word) {
+  word = word.split(" ").join("");
+  var count = {};
+  for(var i = 0; i < word.length; i++){
+    if(word[i] in count){
+      count[word[i]] += 1;
+    }else{
+      count[word[i]] = 1;
+    }
+  }
+  return count;
+}
+
+
+
+console.log(countLetters("abca"))
